25db8da2d37ed58f5e4bc0a3142db2e655f1bdd8
songyewang <wsy1983wsy@163.com>
2016-10-10 17:20:18 +0800

    基本的activity配置

--- app/src/main/java/com/wsy/database/MainActivity.java
+++ app/src/main/java/com/wsy/database/MainActivity.java
@@ -1,7 +1,9 @@
 package com.wsy.database;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onSqliteClicked(View view) {
+        Intent intent = new Intent(this,SqliteActivity.class);
+        startActivity(intent);
+    }
+
+    public void onRealmClicked(View view) {
+        Intent intent = new Intent(this,RealmActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/wsy/database/SqliteActivity.java
@@ -0,0 +1,10 @@
+package com.wsy.database;
+
+import android.app.Activity;
+
+/**
+ * Created by songyewang on 16/10/10.
+ */
+
+public class SqliteActivity extends Activity {
+}
